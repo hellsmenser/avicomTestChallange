8e5c1cc3fff0305c5d646996519bb6ce849da478
hellsmenser <hellsmenser@gmail.com>
2021-04-08 16:59:10 +0300

    Finish SQL

--- SQL/DBbody.sql
+++ SQL/DBbody.sql
@@ -9,14 +9,29 @@ CREATE TABLE Client(
 	name VARCHAR(30) NOT NULL,
 	status VARCHAR(15) NOT NULL,
 	manager VARCHAR(30) NOT NULL,
-	buyedProduct INT NOT NULL,
+	buyedProduct INT,
 	PRIMARY KEY(id)
 );
+
+CREATE TABLE ClientStatus(
+	status VARCHAR(15) NOT NULL,
+	PRIMARY KEY(status)
+);
+
+INSERT INTO ClientStatus(status) VALUES('Ключевой клиент');
+INSERT INTO ClientStatus(status) VALUES('Обычный клиент');
 
 CREATE TABLE Product(
 	id INT NOT NULL IDENTITY(0,1),
 	name VARCHAR(30) NOT NULL,
 	type VARCHAR(8) NOT NULL,
+	price INT NOT NULL,
 	period VARCHAR(7) NOT NULL,
 	PRIMARY KEY(id)
+);
+
+CREATE TABLE BuyedProduct(
+	Client INT NOT NULL,
+	Product INT NOT NULL,
+	PRIMARY KEY(Client, Product)
 );
